f1bd69f8b7ce66e7e4c6ff5c53f05123221c633e
SemihArslanhw <semiharslanhw@gmail.com>
2021-12-10 22:25:47 +0300

    added createing pin

--- backend/Models/Pins.js
+++ backend/Models/Pins.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 
 const PinSchema = new mongoose.Schema({
-    BranchId: {
+    BranchName: {
         type:String,
         require:true,
-        min:3,
         unique:true,
     },
 
